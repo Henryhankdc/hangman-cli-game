8483ecd5a4387de793ca83de4096e6c0efc3b2da
Henry Mesias <henrymesias@gmail.com>
2018-02-13 20:32:37 -0500

    fixed syntax error for choices in inquire statement. added function for choose word

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ var inquirer = require('inquirer');
 var Word  = require('./Word.js');
 var clear = require('clear');
 // Global vars
-var wins = 0;
-var losses = 0;
-var turns;
-var randomWord = [];
-var guesses;
-var checked = [];
-var chooseWord;
-var letter;
+// var wins = 0;
+// var losses = 0;
+// var turns;
+// var randomWord = [];
+// var guesses;
+// var checked = [];
+// var chooseWord;
+// var letter;
 
 var chosenWords;
 var chosenWord;
+var chosenWordObj;
 var chosenCategory;
 var lettersGuessed;
 var lettersRemaining;
@@ -61,12 +62,45 @@ var pickCategory = function() {
         type:'list',
         name: 'category',
         message: "Choose the cateogry you would like to have the theme of the game be.",
-        choises: ['Marvel Cinematic Universe', 'Star Wars']
+        choices: ['Marvel Cinematic Universe', 'Star Wars']
         }
-    ])
+    ]).then(function(response){
+        switch(response.catgeory){
+        case 'Marvel Cinematic Universe':
+            chosenCategory = 'Marvel Cinematic Universe';
+            chooseWord(marvelHeroes);
+            break;
+        case 'Star Wars':
+            chosenCategory = 'Star Wars';
+            chooseWord(starWars);
+            break;
+        }
+    });
+}
 
+// Function to choose the word based on variable and switch statement
+var chooseWord = function(category) {
+    // grab some global vars from the top
+    chosenWord = '';
+    chosenWordObj = {};
+    // head, body, arms(2), legs(2)
+    guessesRemaining = 6;
+    lettersGuessed = [];
+
+    chosenWords = category.slice();
+    var selectRandomWord = Math.floor(Math.random() * currentWords.length);
+    chosenWord = currentWords[selectRandomWord];
+    // use object variable with constructor
+    chosenWordObj = new Word(chosenWord);
+    chosenWords.splice(selectRandomWord, 1);
+    wordsRemaining = chosenWords.length;
+// fire user guess function after selected
+    userGuess();
 
 }
+
+
+
 
 // call welecome function
 gameWelcome();
